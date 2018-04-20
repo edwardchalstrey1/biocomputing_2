9ec4b70228b742b51541b69770134baf71939093
TruptiAG <t.gore@mail.cryst.bbk.ac.uk>
2018-04-20 20:09:52 +0100

    Create biocw.js

---
+++ frontend/BioCW/WWW/JS/biocw.js
@@ -0,0 +1,102 @@
+
+/*  This function disbles  the submit button and rest of the dropdown lists based on the selection */
+function disable(optid){
+	document.getElementById("submit").disabled=false;
+	switch(optid){
+		case "protid":
+			document.getElementById("locid").disabled=true;
+			document.getElementById("accid").disabled=true;
+			document.getElementById("geneid").disabled=true;
+			
+			break;
+		case "locid":
+			document.getElementById("protid").disabled=true;
+			document.getElementById("accid").disabled=true;
+			document.getElementById("geneid").disabled=true;
+			document.getElementById("submit").enabled=true;
+			break;
+		case "geneid":
+			document.getElementById("locid").disabled=true;
+			document.getElementById("accid").disabled=true;
+			document.getElementById("protid").disabled=true;
+			document.getElementById("submit").enabled=true;
+			break;
+		case "accid":
+			document.getElementById("locid").disabled=true;
+			document.getElementById("protid").disabled=true;
+			document.getElementById("geneid").disabled=true;
+			document.getElementById("submit").enabled=true;
+			break;
+}
+}
+/* This function enables  the submit button and rest of the dropdown lists based on the selection */	
+function enable(){
+
+			document.getElementById("locid").disabled=false;
+			document.getElementById("accid").disabled=false;
+			document.getElementById("geneid").disabled=false;
+			document.getElementById("protid").disabled=false;
+			document.getElementById("submit").disabled=true;
+			
+			
+}
+
+function showElement(element)
+{
+   document.getElementById(element).style.display = 'block';
+}
+function hideElement(element)
+{
+   document.getElementById(element).style.display = 'none';
+}
+
+function toggleElement(element)
+{
+   if(document.getElementById(element).style.display == 'none')
+   {
+      showElement(element);
+   }
+   else
+   {
+	   hideElement(element);
+   }
+}
+
+/* After loading the index.html page , this function directs it to the index.cgi */   
+function loadIndex() {
+    document.location = "http://localhost/cgi-bin/index.py";
+}
+
+
+function get_enzyme_name(value){
+	myVar = value;
+	document.getElementById('res_dna').innerHTML=myVar;  
+    
+
+    
+}
+
+/* Accordion */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
+
+    
