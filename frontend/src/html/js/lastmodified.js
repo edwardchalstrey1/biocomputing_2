7c6a267f1a3b58f2553d78ea70ca6734f590009c
TruptiAG <t.gore@mail.cryst.bbk.ac.uk>
2018-05-02 22:36:36 +0100

    Update lastmodified.js

--- frontend/src/html/js/lastmodified.js
+++ frontend/src/html/js/lastmodified.js
@@ -1,9 +1,9 @@
 /* Javascript file to display lastmodified
 Ref: http://www.bioinf.org.uk/teaching/bbk/biocomp2/js/page02.html*/
 document.write("<p class='lastmodified'>",
-               "Last Modified: ", document.lastModified,
+               "Last Modified: ", Date(document.lastModified),
                "</p>");	
-			   /*Hide email : */
+			   
 
 
 	
